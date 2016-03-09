1ccbdad6ce8298396be44a8abd6c5849ca2bba15
alex <alexander.manafi@gmail.com>
2016-03-09 19:40:26 +0100

    dir Task4 was on git ignore list

---
+++ tasks/Task4/Task4/IKleidung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4
+{
+	public interface IKleidung
+	{
+		string Farbe{get; set;}
+		string Marke{get; set;}
+		string Groesse{get; set;}
+		double Preis{get; set;} 
+
+		void UpdatePreis (double newprice);
+	}
+}
---
+++ tasks/Task4/Task4/TShirts.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4
+{
+	public class TShirts : IKleidung
+	{
+		private string m_marke;
+		private string m_groesse;
+		private string m_farbe;
+		private double m_preis;
+
+		public string Marke
+		{
+			get
+			{
+				return m_marke;
+			}
+
+			set
+			{
+				m_marke = value;
+			}
+		}
+
+		public string Farbe
+		{
+			get
+			{
+				return m_farbe;
+			}
+
+			set
+			{
+				m_farbe = value;
+			}
+		}
+
+		public string Groesse
+		{
+			get
+			{
+				return m_groesse;
+			}
+
+			set
+			{
+				m_groesse = value;
+			}
+		}
+
+		public double Preis
+		{
+			get
+			{
+				return m_preis;
+			}
+
+			set
+			{
+				if (value < 0) throw new Exception ("Preis darf nicht negativ sein.");
+				m_preis = value;
+			}
+		}
+
+
+		public TShirts(string Marke, string Farbe, string Groesse, double Preis)
+		{
+			if (Preis < 0) throw new ArgumentOutOfRangeException ("Preis darf nicht kleiner 0 sein.");
+
+			m_marke = Marke; 
+			m_farbe = Farbe;
+			m_groesse = Groesse;
+			m_preis = Preis;
+		}
+
+		public void UpdatePreis(double newprice)
+		{
+			if (newprice < 0) throw new ArgumentOutOfRangeException ("Preis darf nicht kleiner 0 sein.");
+
+			m_preis = newprice;
+		}
+	}
+}
+
