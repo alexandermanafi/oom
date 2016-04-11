cfb6fd44a8aa9dc6847b6038d8848f2611dc3417
alex <alexander.manafi@gmail.com>
2016-04-11 16:29:19 +0200

    task 6 observable/observer hinzugefügt

---
+++ tasks/Task_6/Task_6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.IO;
+using System.Threading;
+
+namespace Task6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Variablendefiniton
+			char ex;
+
+			//"stream-bares" .txt generieren für observable
+			TextWriter tw = new StreamWriter(@"/home/alex/stream.txt");
+
+			do
+			{
+				Console.WriteLine("Timestamps werden generiert");
+				//jede sekunde timestamps erzeugen
+				var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Timestamp();
+
+				//timestamps ausgeben
+				using (observable.Subscribe(
+					x => Console.WriteLine("{0}: {1}", x.Value, x.Timestamp)))
+
+				//timestamps in stream.txt streamen
+				using (observable.Subscribe(
+					x => tw.WriteLine(Convert.ToString(x.Timestamp))))
+
+				{
+					Console.WriteLine("Zum Abbrechen Eingabe");
+					Console.ReadKey();
+				}
+
+				ex = Convert.ToChar(Console.ReadKey().KeyChar);
+			}
+			while (ex.Equals(" "));
+			tw.Close();
+		}
+	}    
+}
